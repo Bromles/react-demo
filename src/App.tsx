445fc1fe765bb48fed11afa0835b5632e0059171
Bromles <alexandr.v.u@gmail.com>
2022-02-28 17:30:35 +0300

    Add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navigation/Navbar';
-import Currency from './pages/currency/Currency';
-import MeasurementsUnits from './pages/measurements-units/MeasurementsUnits';
+import Home from './pages/home/Home';
 
 const App = () => {
+    const Currency = React.lazy(() => import('./pages/currency/Currency'));
+    const MeasurementsUnits = React.lazy(() => import('./pages/measurements-units/MeasurementsUnits'));
+
     return (
         <>
             <Navbar/>
-            <Routes>
-                <Route path="/currency" element={ <Currency/> }/>
-                <Route path="/mu" element={ <MeasurementsUnits/> }/>
-            </Routes>
+            <React.Suspense fallback={ <span>Loading...</span> }>
+                <Routes>
+                    <Route path="/" element={ <Home/> }/>
+                    <Route path="/currency" element={ <Currency/> }/>
+                    <Route path="/mu" element={ <MeasurementsUnits/> }/>
+                </Routes>
+            </React.Suspense>
         </>
     );
 };
